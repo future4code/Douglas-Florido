75533f264bdcde7ad0db1f8c9574b348f4b9b183
DouglasFlorido <crascaroth@gmail.com>
2021-01-14 21:08:25 -0300

    faltando apenas o SpaceX, está fora do ar

--- Semana7/Aula4/pokeboredspacex/src/Páginas/Bored.js
+++ Semana7/Aula4/pokeboredspacex/src/Páginas/Bored.js
@@ -2,19 +2,46 @@ import React from 'react'
 import axios from 'axios'
 
 export class Bored extends React.Component {
+    state = {
+        atividade: '',
+        qtdePessoas: '',
+        tipo: '',
+        acessibilidade: '',
+        preco: ''
+    }
 
     listarBored = () =>{
         axios.get('http://www.boredapi.com/api/activity/')
        .then((res) => {
            console.log(res)
+           this.setState({atividade: res.data.activity})
+           this.setState({qtdePessoas: res.data.participants})
+           this.setState({tipo: res.data.type})
+           this.setState({acessibilidade: res.data.accessibility})
+           this.setState({preco: res.data.price})
+           
+
        }).catch((err) => {
            console.log(err)
        })
        }
 
+    componentDidMount = () => {
+        this.listarBored();
+    };
+
+     
+
     render(){
+        const componente = this.listarBored
     return(
-        <div>bored</div>
+        <div>        
+        <p>Atividade: {this.state.atividade}</p>
+        <p>Quantidade de Pessoas: {this.state.qtdePessoas}</p>
+        <p>Tipo: {this.state.tipo}</p>
+        <p>Acessibilidade: {this.state.acessibilidade}</p>
+        <p>Preço: {this.state.preco}</p>
+        </div>
     )
     }
 }
--- Semana7/Aula4/pokeboredspacex/src/Páginas/Poke.js
+++ Semana7/Aula4/pokeboredspacex/src/Páginas/Poke.js
@@ -3,18 +3,52 @@ import axios from 'axios'
 
 
 export class Poke extends React.Component {
+    state = {
+        pokemons: [],
+        pokeNome: '',
+        pokeFoto: ''
+    }
 
     listarPoke = () =>{
-     axios.get('')
+     axios.get('https://pokeapi.co/api/v2/pokemon/')
     .then((res) => {
         console.log(res)
+        this.setState({pokemons: res.data.results})
+        
+        
     }).catch((err) => {
         console.log(err)
     })
     }
+
+    // pegarFotoPoke = (nome) => {
+    //     axios.get(`https://pokeapi.co/api/v2/pokemon/${nome}`)
+    //     .then((res) => {
+    //         this.setState({pokeFoto: res.data.sprites.front_default})
+    //     })
+    //     .catch((err) => console.log(err))
+    // }
+
+    
+    componentDidMount = () => {
+        this.listarPoke();
+    };
     render(){
+        
     return(
-        <div>{this.listarPoke()}</div>
+        <div>
+        {this.state.pokemons.map((pk) => {
+            return (
+                // const foto = this.pegarFotoPoke(pk.name)
+
+                <div>
+                    <p>{pk.name}</p>
+                    
+                                  
+                </div>
+            )
+        })}
+        </div>
     )
     }
 }
