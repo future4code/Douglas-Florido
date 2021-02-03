36cb5b4271f89da7b06ecf9633266884f9c768ae
DouglasFlorido <crascaroth@gmail.com>
2021-02-02 23:11:52 -0300

    esqueleto quase pronto

---
+++ Semana10/Aula2/labex/src/Components/CreateAccount/CreateAccount.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function CreateAccount() {
+  return (
+    <div>
+      CreateACcount
+    </div>
+  );
+}
+
+export default CreateAccount;
---
+++ Semana10/Aula2/labex/src/Components/MainPage/MainPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../Header/Header.js'
+import ListTrips from '../ListTrips/ListTrips.js'
+
+function MainPage() {
+  return (
+    <div>
+      <Header/>
+      <ListTrips/>
+    </div>
+  );
+}
+
+export default MainPage;
