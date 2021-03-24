e64431a712102491f4a7c2aee9624d3c87fe5a21
DouglasFlorido <crascaroth@gmail.com>
2021-03-24 16:39:13 -0300

    exercicio iniciado

---
+++ Semana15/Aula3/Exercicios/src/index.ts
@@ -0,0 +1,13 @@
+import app from './app'
+import connection from './connection'
+
+
+const getActorById = async (id: string): Promise<any> => {
+    const result = await connection.raw(`
+      SELECT * FROM Actor WHERE id = '${id}'
+    `)
+  
+      return result[0][0]
+  }
+  
+  console.log( getActorById("001") )
