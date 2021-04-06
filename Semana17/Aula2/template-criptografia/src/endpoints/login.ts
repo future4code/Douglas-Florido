ecb4d5ffb0a87f127f8e465ab99c09e1461a578c
DouglasFlorido <crascaroth@gmail.com>
2021-04-06 16:09:55 -0300

    Feito até o exercicio 3

---
+++ Semana17/Aula2/template-criptografia/src/endpoints/login.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from "express"
+import connection from "../connection"
+import { generateToken } from "../services/authenticator"
+import {compare} from '../services/encryption'
+
+export default async function login(
+   req: Request,
+   res: Response
+): Promise<void> {
+   try {
+
+      const { email, password } = req.body
+
+      if (!email || !password) {
+         res.statusCode = 422
+         throw new Error("'email' e 'password' são obrigatórios")
+      }
+
+      const [user] = await connection("to_do_list_users").where({ email })
+
+
+      if (!compare(password,user.password) ) {
+         res.statusCode = 401
+         throw new Error("Credenciais inválidas")
+      }
+
+      
+      const token = generateToken({
+         id: user.id
+      })
+
+      res.send({ token })
+
+   } catch (error) {
+
+      if (res.statusCode === 200) {
+         res.status(500).send({ message: "Internal server error" })
+      } else {
+         res.send({ message: error.message })
+      }
+   }
+}
